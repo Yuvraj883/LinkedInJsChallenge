c10b4bb496973edfdd90ffe8208daff5d48850ff
Yuvraj883 <kingyuvi88@gmail.com>
2022-07-31 12:56:35 +0530

    Js logic completed

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+const second = 1000; 
+const minute = 60*second; 
+const hour = minute*60; 
+const day = hour*24; 
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+const currentTime = new Date(); 
+const lastSeen = new Date(2022,6,31,12,35,0); 
+const diff =(currentTime.getTime() - lastSeen.getTime()); 
+console.log(diff);
+const h2 = document.getElementById("header");
+const para = document.getElementById("para"); 
+ para.innerText = 	`Current Time = ${currentTime} 
+ Last Seen = ${lastSeen}
+ diff = ${currentTime-lastSeen}`
+//h2.innerText = diff; 
+if(diff===0){
+h2.innerText = "Online"; 
+}
+else if(diff<= minute){
+h2.innerText = "Last seen just now"+diff; 
+}
+else if(diff> minute && diff<hour){
+h2.innerText = `last seen ${Math.floor(diff/(1000*60))} minutes ago`; 
+}
+else if(diff>hour && diff<day){
+h2.innerText = `last seen ${Math.floor(diff/(1000*60*60))} hours ago`;
+}
+else if(diff>day && diff<day*30){
+h2.innerText = `last seen ${Math.floor(diff/(day))} days ago`; 
+}
+else if(diff>day*30){
+h2.innerText = "Last Seen more than a month ago"; 
+}
+else{
+  h2.innerText = "None of the conditions match"; 
+}
+// let diff = currentTime-lastSeen; 
+// if(currentTime-lastSeen === 0){
+//   console.log("Online"); 
+// }else if(currentTime-lastSeen<=1000*60){
+// console.log("Last seen just now");
+// }else if(currentTime-lastSeen>=1000*60 && currentTime-lastSeen)
