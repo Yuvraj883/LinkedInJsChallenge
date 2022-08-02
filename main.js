8780f6635cdec72bcc73f8a1d184f14a1c833105
Yuvraj883 <kingyuvi88@gmail.com>
2022-08-03 00:46:34 +0530

    Minor changes

--- main.js
+++ main.js
@@ -34,18 +34,17 @@ function findLastSeen(){
   if(diff===0){
     h2.innerText = `online`;
   }
-  else if(diff<minute){
+  else if(diff>0 && diff<minute){
   h2.innerText = "last seen just now"; 
   }
   else if(diff>= minute && diff<2*minute){
   h2.innerText = "last seen a minute ago"; 
   }
   else if(diff> minute && diff<hour){
-  h2.innerText = `last seen ${Math.floor(diff/(1000*60))} minutes ago`; 
+  h2.innerText = `last seen ${Math.floor(diff/(minute))} minutes ago`; 
   }
   else if(diff>hour && diff<day){
-  h2.innerText = `last seen ${Math.floor(diff/(1000*60*60))} hours ago at 
-  ${lastSeen.getHours()+":"+ lastSeen.getMinutes()}hrs `;
+  h2.innerText = `last seen ${Math.floor(diff/(hour))} hours ago at ${lastSeen.getHours()+":"+ lastSeen.getMinutes()}hrs `;
   }
   else if(diff>day && diff<day*30){
   h2.innerText = `last seen ${Math.floor(diff/(day))} days ago`; 
